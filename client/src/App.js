dd7afccf654acdf194482405cb42744a6ae2c46a
Muhammad Khairi <khairi.khrdin@gmail.com>
2022-02-13 22:28:36 +0800

    Convert datetime to timestamp

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,19 @@ import "./App.css";
 import Axios from "axios";
 import Map from "./map.js";
 import { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import DateTimePicker from "react-datetime-picker";
+// import "flatpickr/dist/themes/material_green.css";
+// import Flatpickr from "react-flatpickr";
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [longitude, setLongitude] = useState(101.9758);
   const [latitude, setLatitude] = useState(4.2105);
+  const [datetime, setDateTime] = useState(new Date());
+  const [inputValue, setInputValue] = useState("");
 
   const getLocation = async () => {
     setLoading(true);
@@ -15,9 +23,18 @@ function App() {
       "https://api.wheretheiss.at/v1/satellites/25544"
     );
 
+    // const res1 = await Axios.get("http://api.open-notify.org/astros.json");
+
     console.log(res);
+    // console.log(res1);
 
     const { longitude, latitude } = await res.data;
+    // let people = await res.data.people;
+
+    // let arr = [];
+    // Object.keys(people).forEach(function(key) {
+    //   arr.push(people[key]);
+    // });
 
     let tempLng = parseFloat(longitude);
     let tempLat = parseFloat(latitude);
@@ -31,6 +48,25 @@ function App() {
     setLoading(false);
   };
 
+  let onChange = (event) => {
+    const newValue = event.target.value;
+    let testVal = Math.floor(newValue.getTime() / 1000);
+    console.log(testVal);
+    setInputValue(newValue);
+  };
+
+  async function postDate(e) {
+    e.preventDefault();
+
+    try {
+      await Axios.post("http://localhost:3001/post_date", {
+        datetime,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     getLocation();
   }, []);
@@ -38,12 +74,52 @@ function App() {
   return (
     <div className="App">
       {!loading ? (
-        <Map
-          center={{
-            lat: latitude,
-            lng: longitude,
-          }}
-        />
+        <center>
+          <Map
+            center={{
+              lat: latitude,
+              lng: longitude,
+            }}
+            zoom={6.5}
+          />
+          <p>
+            <form onSubmit={postDate}>
+              <DateTimePicker
+                onChange={(datetime) => {
+                  setDateTime(datetime);
+                  let testVal = Math.floor(datetime.getTime() / 1000);
+                  console.log(testVal);
+                }}
+                value={datetime}
+              />
+              {/* <Flatpickr
+                data-enable-time
+                key={datetime}
+                value={datetime}
+                onChange={onChange}
+                options={{
+                  // maxDate: "today",
+                  time_24hr: "true",
+                }}
+              /> */}
+
+              {/* <DatePicker
+                selected={datetime}
+                onChange={(datetime) => {
+                  setDateTime(datetime);
+                  return datetime;
+                }}
+                showTimeSelect
+              /> */}
+              {/* <input placeholder="testing here boi.." onChange={onChange} /> */}
+              {/* <p>{inputValue}</p> */}
+              <p>{Math.floor(datetime.getTime() / 1000).toString()}</p>
+              <button onCLick="update" type="submit">
+                Find Date
+              </button>
+            </form>
+          </p>
+        </center>
       ) : (
         <h1>Loading</h1>
       )}
--- server/index.js
+++ server/index.js
@@ -4,6 +4,14 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+
+app.post("/post_date", async (req, res) => {
+  let { datetime } = req.body;
+
+  datetime = Math.floor(datetime / 1000);
+  console.log(datetime);
+});
+
 app.listen(3001, () => {
   console.log("Test");
 });
